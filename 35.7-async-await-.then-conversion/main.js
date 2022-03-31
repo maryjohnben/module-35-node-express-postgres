9fe7505bb2aee1b1bbd0168c713702f18a6e0a3a
Mary Benjamin <marybenjamin713@gmail.com>
2022-03-30 21:01:32 -0500

    async files added

---
+++ 35.7-async-await-.then-conversion/main.js
@@ -0,0 +1,24 @@
+const {
+    generateMessage,
+    goodbye,
+    generateSlogan,
+  } = require("../utils/slogan-generator");
+  
+  
+  async function getSlogan(request) {
+    const response = await generateSlogan(request);
+    console.log(`Your request was: ${request}`);
+      console.log(`Your slogan is: ${response.slogan}`)
+  }
+  
+  
+  
+  async function fullSession(request) {
+    const response = await generateMessage();
+    console.log(response);
+    const slogan = await getSlogan(request);
+    const bye = await goodbye()
+    console.log(bye);
+  }
+  
+  module.exports = { getSlogan, fullSession };
